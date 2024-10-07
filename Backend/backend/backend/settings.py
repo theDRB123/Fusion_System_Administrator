7eab2b6961387f1811a17101252f98fa4a94a965
Kaarti22 <kaartimondi@gmail.com>
2024-10-07 22:45:33 +0530

    django existing models setup

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fusionlab',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
