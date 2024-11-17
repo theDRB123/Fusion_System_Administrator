744e592c9fea58b5227d9228c45f426cb41cc3d0
idlidosa1206 <northkamat@gmail.com>
2024-11-17 13:49:27 +0530

    Updated authcontext to handle tokens

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -4,12 +4,24 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({children}) =>{
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const login =()=> setIsAuthenticated(true);
-    const logout =()=> setIsAuthenticated(false);
+    const [authToken, setAuthToken] = useState(null); 
+    
+    const login = (token) => {
+        setIsAuthenticated(true);
+        setAuthToken(token);
+        localStorage.setItem("authToken", token); 
+      };
+
+      const logout = () => {
+        setIsAuthenticated(false);
+        setAuthToken(null);
+        localStorage.removeItem("authToken"); 
+      };
+
     return (
-        <AuthContext.Provider value = {{isAuthenticated, login, logout}}>
-            {children}
-        </AuthContext.Provider>
+        <AuthContext.Provider value={{ isAuthenticated, authToken, login, logout }}>
+        {children}
+      </AuthContext.Provider>
     );
 };
 
--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -10,7 +10,7 @@ const LoginPage = ()=>{
     const {login}= useAuth();
     const navigate = useNavigate();
 
-    const handleLogin = (e)=>{
+    const handleLogin = async(e)=>{
         e.preventDefault();
         try{
           const response = await axios.post("http://127.0.0.1:8000/api/login/", {
