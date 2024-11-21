06ce1af2d3ea8b7ef3522aad3c4d3839d38100df
SamakshAgarwal1112 <agarwalsamaksh11@gmail.com>
2024-11-22 01:20:30 +0530

    Bug fixes

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -115,15 +115,6 @@ DATABASES = {
     }
 }
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-}
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
