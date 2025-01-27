77b238a9c67f7ba03546a309994c56b9d3d87be6
Mondi Venkata Kartikeya <129477209+Kaarti22@users.noreply.github.com>
2025-01-27 17:24:29 +0530

    Integrated add student API with frontend (#41)
    * bug fix
    
    * integrated add student api with frontend

--- client/src/pages/UserManagementPages/StudentCreationPage.jsx
+++ client/src/pages/UserManagementPages/StudentCreationPage.jsx
@@ -1,296 +1,255 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
-    TextInput,
-    Select,
-    Button,
-    Grid,
-    Group,
-    Box,
-    NumberInput,
-    Radio,
-    rem,
-    Title,
-    Divider,
-    Progress,
-    Flex,
-    Paper,
-} from '@mantine/core';
-import { FaCheck, FaTimes } from 'react-icons/fa';
-import { notifications, showNotification } from '@mantine/notifications';
-import { DateInput, YearPickerInput } from '@mantine/dates';
+  TextInput,
+  Select,
+  Button,
+  Grid,
+  Group,
+  Box,
+  NumberInput,
+  Radio,
+  rem,
+  Title,
+  Divider,
+  Progress,
+  Flex,
+  Paper,
+} from "@mantine/core";
+import { FaCheck, FaTimes } from "react-icons/fa";
+import { notifications, showNotification } from "@mantine/notifications";
+import { DateInput, YearPickerInput } from "@mantine/dates";
 import { useMediaQuery } from "@mantine/hooks";
+import { createStudent } from "../../api/Users";
 
 const StudentCreationPage = () => {
-    const xIcon = <FaTimes style={{ width: rem(20), height: rem(20) }} />;
-    const checkIcon = <FaCheck style={{ width: rem(20), height: rem(20) }} />;
+  const xIcon = <FaTimes style={{ width: rem(20), height: rem(20) }} />;
+  const checkIcon = <FaCheck style={{ width: rem(20), height: rem(20) }} />;
 
-    const [formValues, setFormValues] = useState({
-        rollNo: '',
-        firstName: '',
-        lastName: '',
-        gender: '',
-        dob: null,
-        address: '',
-        phone: '',
-        batch: '',
-        category: '',
-        fatherName: '',
-        motherName: '',
-        hostel: '',
-    });
+  const [formValues, setFormValues] = useState({
+    roll_no: "",
+    first_name: "",
+    last_name: "",
+    sex: "",
+    category: "",
+    father_name: "",
+    mother_name: "",
+  });
 
-    const [progress, setProgress] = useState(0);
+  const [progress, setProgress] = useState(0);
 
-    useEffect(() => {
-        const totalFields = Object.keys(formValues).length;
-        const filledFields = Object.values(formValues).filter((value) => value).length;
-        setProgress((filledFields / totalFields) * 100);
-    }, [formValues]);
+  useEffect(() => {
+    const totalFields = Object.keys(formValues).length;
+    const filledFields = Object.values(formValues).filter(
+      (value) => value
+    ).length;
+    setProgress((filledFields / totalFields) * 100);
+  }, [formValues]);
 
-    const handleChange = (field, value) => {
-        setFormValues((prev) => ({ ...prev, [field]: value }));
+  const handleChange = (field, value) => {
+    setFormValues((prev) => ({
+      ...prev,
+      [field]: field === "hall_no" ? parseInt(value, 10) || 0 : value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Form Submitted", formValues);
+
+    try {
+      await createStudent(formValues);
+      showNotification({
+        icon: checkIcon,
+        title: "Success",
+        position: "top-center",
+        withCloseButton: true,
+        autoClose: 5000,
+        message: "Student Created Successfully.",
+        color: "green",
+      });
+      setFormValues({
+        roll_no: "",
+        first_name: "",
+        last_name: "",
+        sex: "",
+        category: "",
+        father_name: "",
+        mother_name: "",
+      });
+    } catch (err) {
+      const errorMessage = err.response
+        ? `${JSON.stringify(err.response.data.error) || JSON.stringify(err.response.data.data) || JSON.stringify(err.response.data.message)}`
+        : err.request
+        ? "No response received from the server"
+        : `${err.message}`;
+
+      showNotification({
+        title: "Error",
+        icon: xIcon,
+        position: "top-center",
+        withCloseButton: true,
+        message: errorMessage,
+        color: "red",
+      });
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        handleSubmit();
+      }
     };
 
-    const handleSubmit = () => {
-        console.log('Form Submitted', formValues);
-        showNotification({
-            icon: checkIcon,
-            title: "Success",
-            position: "top-center",
-            withCloseButton: true,
-            autoClose: 5000,
-            message: "Student Created Successfully.",
-            color: "green",
-        });
+    window.addEventListener("keydown", handleKeyDown);
 
-        showNotification({
-            title: 'Error',
-            icon: xIcon,
-            position: "top-center",
-            withCloseButton: true,
-            message: 'An error occurred.',
-            color: 'red',
-        });
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
     };
+  }, [formValues]);
 
-    useEffect(() => {
-        const handleKeyDown = (event) => {
-            if (event.key === 'Enter') {
-                event.preventDefault();
-                handleSubmit();
-            }
-        };
+  const matches = useMediaQuery("(min-width: 768px)");
 
-        window.addEventListener('keydown', handleKeyDown);
+  return (
+    <Box maw={700} mx="auto" p="lg" shadow="sm" withBorder>
+      <Paper shadow="xl" radius="lg" p="xl">
+        <Flex
+          gap="md"
+          justify="center"
+          align="center"
+          direction="row"
+          wrap="wrap"
+        >
+          <Button
+            variant="gradient"
+            size="xl"
+            radius="xs"
+            gradient={{ from: "blue", to: "cyan", deg: 90 }}
+            w={matches && "500px"}
+            style={{
+              fontSize: "1.8rem",
+              lineHeight: 1.2,
+              marginBottom: "1rem",
+            }}
+          >
+            <Title
+              order={1}
+              align="center"
+              style={{
+                fontSize: "1.25rem",
+                wordBreak: "break-word",
+              }}
+            >
+              Add Student
+            </Title>
+          </Button>
+        </Flex>
 
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        };
-    }, [formValues]);
+        <Divider my="sm" />
 
-    const matches = useMediaQuery('(min-width: 768px)');
+        {/* Progress Bar */}
+        <Progress value={progress} color="blue" mb="md" />
 
-    return (
-        <Box maw={700} mx="auto" p="lg" shadow="sm" withBorder>
-            <Paper shadow="xl" radius="lg" p="xl">
-                <Flex
-                    gap="md"
-                    justify="center"
-                    align="center"
-                    direction="row"
-                    wrap="wrap"
-                >
-                    <Button
-                        variant="gradient"
-                        size="xl"
-                        radius="xs"
-                        gradient={{ from: "blue", to: "cyan", deg: 90 }}
-                        w={matches && "500px"}
-                        style={{
-                            fontSize: "1.8rem",
-                            lineHeight: 1.2,
-                            marginBottom: "1rem",
-                        }}
-                    >
-                        <Title
-                            order={1}
-                            align="center"
-                            style={{
-                                fontSize: "1.25rem",
-                                wordBreak: "break-word",
-                            }}
-                        >
-                            Add Student
-                        </Title>
-                    </Button>
-                </Flex>
+        <Grid gutter="md">
+          {/* Roll No */}
+          <Grid.Col span={6}>
+            <TextInput
+              label="Roll No"
+              placeholder="Enter roll number"
+              value={formValues.roll_no}
+              onChange={(e) => handleChange("roll_no", e.target.value)}
+              required
+            />
+          </Grid.Col>
 
-                <Divider my="sm" />
+          {/* First Name */}
+          <Grid.Col span={6}>
+            <TextInput
+              label="First Name"
+              placeholder="Enter first name"
+              value={formValues.first_name}
+              onChange={(e) => handleChange("first_name", e.target.value)}
+              required
+            />
+          </Grid.Col>
 
-                {/* Progress Bar */}
-                <Progress value={progress} color="blue" mb="md" />
+          {/* Last Name */}
+          <Grid.Col span={6}>
+            <TextInput
+              label="Last Name"
+              placeholder="Enter last name / NA"
+              value={formValues.last_name}
+              onChange={(e) => handleChange("last_name", e.target.value)}
+              required
+            />
+          </Grid.Col>
 
-                <Grid gutter="md">
-                    {/* Roll No */}
-                    <Grid.Col span={6}>
-                        <TextInput
-                            label="Roll No"
-                            placeholder="Enter roll number"
-                            value={formValues.rollNo}
-                            onChange={(e) => handleChange('rollNo', e.target.value)}
-                            required
-                        />
-                    </Grid.Col>
+          {/* Gender */}
+          <Grid.Col span={12}>
+            <Radio.Group
+              label="Gender"
+              value={formValues.sex}
+              onChange={(value) => handleChange("sex", value)}
+              required
+            >
+              <Group spacing="sm" position="apart" mt="xs">
+                <Radio value="male" label="Male" />
+                <Radio value="female" label="Female" />
+                <Radio value="other" label="Other" />
+              </Group>
+            </Radio.Group>
+          </Grid.Col>
 
-                    {/* First Name */}
-                    <Grid.Col span={6}>
-                        <TextInput
-                            label="First Name"
-                            placeholder="Enter first name"
-                            value={formValues.firstName}
-                            onChange={(e) => handleChange('firstName', e.target.value)}
-                            required
-                        />
-                    </Grid.Col>
+          {/* Category */}
+          <Grid.Col span={12}>
+            <Select
+              label="Category"
+              placeholder="Select category"
+              data={["General", "OBC", "SC", "ST"]}
+              value={formValues.category}
+              onChange={(value) => handleChange("category", value)}
+              required
+            />
+          </Grid.Col>
 
-                    {/* Last Name */}
-                    <Grid.Col span={6}>
-                        <TextInput
-                            label="Last Name"
-                            placeholder="Enter last name"
-                            value={formValues.lastName}
-                            onChange={(e) => handleChange('lastName', e.target.value)}
-                            required
-                        />
-                    </Grid.Col>
+          {/* Father's Name */}
+          <Grid.Col span={6}>
+            <TextInput
+              label="Father's Name"
+              placeholder="Enter father's name"
+              value={formValues.father_name}
+              onChange={(e) => handleChange("father_name", e.target.value)}
+              required
+            />
+          </Grid.Col>
 
-                    {/* Gender */}
-                    <Grid.Col span={12}>
-                        <Radio.Group
-                            label="Gender"
-                            value={formValues.gender}
-                            onChange={(value) => handleChange('gender', value)}
-                            required
-                        >
-                            <Group spacing="sm" position="apart" mt="xs">
-                                <Radio value="male" label="Male" />
-                                <Radio value="female" label="Female" />
-                                <Radio value="other" label="Other" />
-                            </Group>
-                        </Radio.Group>
-                    </Grid.Col>
-
-                    {/* Date of Birth */}
-                    <Grid.Col span={6}>
-                        <DateInput
-                            value={formValues.dob}
-                            onChange={(value) => handleChange('dob', value)}
-                            label="Date of Birth"
-                            placeholder="Pick a date"
-                            required
-                        />
-                    </Grid.Col>
-
-                    {/* Address */}
-                    <Grid.Col span={12}>
-                        <TextInput
-                            label="Address"
-                            placeholder="Enter address"
-                            value={formValues.address}
-                            onChange={(e) => handleChange('address', e.target.value)}
-                        />
-                    </Grid.Col>
-
-                    {/* Phone Number */}
-                    <Grid.Col span={6}>
-                        <NumberInput
-                            label="Phone Number"
-                            placeholder="Enter phone number"
-                            value={formValues.phone}
-                            onChange={(value) => handleChange('phone', value)}
-                            hideControls
-                        />
-                    </Grid.Col>
-
-                    {/* Batch */}
-                    <Grid.Col span={6}>
-                        <TextInput
-                            label="Batch"
-                            placeholder="Enter batch"
-                            value={formValues.batch}
-                            onChange={(e) => handleChange('batch', e.target.value)}
-                            required
-                        />
-                    </Grid.Col>
-
-                    {/* Category */}
-                    <Grid.Col span={12}>
-                        <Select
-                            label="Category"
-                            placeholder="Select category"
-                            data={['General', 'OBC', 'SC', 'ST']}
-                            value={formValues.category}
-                            onChange={(value) => handleChange('category', value)}
-                            required
-                        />
-                    </Grid.Col>
-
-                    {/* Father's Name */}
-                    <Grid.Col span={6}>
-                        <TextInput
-                            label="Father's Name"
-                            placeholder="Enter father's name"
-                            value={formValues.fatherName}
-                            onChange={(e) => handleChange('fatherName', e.target.value)}
-                            required
-                        />
-                    </Grid.Col>
-
-                    {/* Mother's Name */}
-                    <Grid.Col span={6}>
-                        <TextInput
-                            label="Mother's Name"
-                            placeholder="Enter mother's name"
-                            value={formValues.motherName}
-                            onChange={(e) => handleChange('motherName', e.target.value)}
-                            required
-                        />
-                    </Grid.Col>
-
-                    {/* Hostel. */}
-                    <Grid.Col span={6}>
-                        <Select
-                            label="Hostel"
-                            placeholder="Select allotted hostel"
-                            data={['Maa Saraswati Hostel', 'Nagarjuna Hostel', 'Aryabhatta Hostel', 'Vivekananda Hostel', 'Vasishtha Hostel', 'Panini Hostel']}
-                            value={formValues.hostel}
-                            onChange={(value) => handleChange('hostel', value)}
-                        />
-                    </Grid.Col>
-                </Grid>
-                {/* <YearPickerInput
-                    label="Pick date"
-                    placeholder="Pick date"
-                    value={formValues.batch}
-                    onChange={(e) => handleChange('batch', e.target.value)}
-                /> */}
-
-                {/* Create Button */}
-                <Flex
-                    gap="md"
-                    justify="center"
-                    align="center"
-                    direction="row"
-                    wrap="wrap"
-                    mt="xl"
-                >
-                    <Button onClick={handleSubmit} color="blue" size="md">
-                        Add Student
-                    </Button>
-                </Flex>
-            </Paper>
-        </Box>
-    );
+          {/* Mother's Name */}
+          <Grid.Col span={6}>
+            <TextInput
+              label="Mother's Name"
+              placeholder="Enter mother's name"
+              value={formValues.mother_name}
+              onChange={(e) => handleChange("mother_name", e.target.value)}
+              required
+            />
+          </Grid.Col>
+        </Grid>
+        <Flex
+          gap="md"
+          justify="center"
+          align="center"
+          direction="row"
+          wrap="wrap"
+          mt="xl"
+        >
+          <Button onClick={handleSubmit} color="blue" size="md">
+            Add Student
+          </Button>
+        </Flex>
+      </Paper>
+    </Box>
+  );
 };
 
 export default StudentCreationPage;
